f9a7acb9675f8a3ed160fbe15bd39491fbe5a810
Sean Kelly <sean_kelly95@hotmail.com>
2019-04-14 15:44:18 -0500

    db config

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = {
     mongoURI:
-      'mongodb+srv://AlphaUser:Tree627-Rock36@surgenorteam-wdw0t.mongodb.net/test?retryWrites=true'
+      'mongodb+srv://AlphaUser:Tree627%2DRock36@surgenorteam-wdw0t.mongodb.net/test?retryWrites=true'
   };
 } else {
   module.exports = { mongoURI: 'mongodb://localhost/surgenorteam-dev' };
